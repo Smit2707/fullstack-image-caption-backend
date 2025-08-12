2e5bfdb45263b10294e7565e23c336960110da9e
Smit Yadav <Smityadav2707@gmail.com>
2025-08-12 11:01:26 +0530

    impemented cors

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const authRoute = require("./routes/auth.routes")
 const postRoute = require("./routes/post.routes")
 const cookieParser = require("cookie-parser")
-
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
@@ -10,5 +10,8 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
-
+app.use(cors({
+    origin: "http://localhost:5173", // your frontend URL
+    credentials: true
+}));
 module.exports = app;
