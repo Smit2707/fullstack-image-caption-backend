2e81574c7f2b3f0fe0cde4dc8392f290fa75e892
Smit Yadav <Smityadav2707@gmail.com>
2025-08-12 11:45:55 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ const cookieParser = require("cookie-parser")
 const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
-app.use(cors());
 module.exports = app;
